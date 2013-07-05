7dfd3bb5098888be2fe9481c6c47c5a33b84ae3d
katebron <kate.bronstad@tufts.edu>
2013-07-05 15:26:10 -0400

    redid changing logo, reassigned ids to first sidebar, styled first sidebar, took off fixed height on highlights, increased range of vertically stacked highlights up to 750px

--- drupal/sites/all/themes/selbert/templates/region--sidebar-first.tpl.php
+++ drupal/sites/all/themes/selbert/templates/region--sidebar-first.tpl.php
@@ -28,40 +28,40 @@
 ?>
 
   <section class="<?php print $classes; ?>">
-  	<?php if ($is_front): ?>
-  	<div id = "frontLeftSide">
-      <div>
-  		<h3>Today's Hours</h3>
-  		<p>8:30am - 10:00pm</p>
-		<p>
-		<a href="http://www.library.tufts.edu/tisch/hours.htm">More Hours</a></p>
+    <?php if ($is_front): ?>
+    <div id = "frontLeftSide">
+      <div class="frontSideBlock frontHours">
+      <h3>Today's Hours</h3>
+      <p>8:30am - 10:00pm</p>
+    <p>
+    <a href="http://www.library.tufts.edu/tisch/hours.htm">More Hours</a></p>
        </div>
-      <div>
+      <div class="frontSideBlock sideAsk">
         <h3>Ask a Librarian</h3>
-    	<ul>
-		<li>Chat ></li>
-		<li>Email > </li>
-		<li>In person > </li>
-		<li>Phone > </li>
-		</ul>
-	   </div>
-        <div>
-    	<h3>Available Technology</h3>
+      <ul>
+    <li>Chat ></li>
+    <li>Email > </li>
+    <li>In person > </li>
+    <li>Phone > </li>
+    </ul>
+     </div>
+        <div class="frontSideBlock techAvail">
+      <h3>Available Technology</h3>
         <ul>
-		<li>xx Bikes</li>
-		<li>xx External Hard Drives</li>
-		<li>xx iPads</li>
-		<li>xx Laptops</li>
-		<li>xx Video Cameras</li>
-		<li>xx Workstations</li>
-		</ul>
+    <li>xx Bikes</li>
+    <li>xx External Hard Drives</li>
+    <li>xx iPads</li>
+    <li>xx Laptops</li>
+    <li>xx Video Cameras</li>
+    <li>xx Workstations</li>
+    </ul>
         </div>
-         <div>
-    	<h3>Available Study Rooms</h3>
-    	<p>xx Group Study Rooms</p>
+         <div class="frontSideBlock roomAvail">
+      <h3>Available Study Rooms</h3>
+      <p>xx Group Study Rooms</p>
         </div>    
-    </div>	
-  	
+    </div>  
+    
     <?php print $content; ?>
   </section><!-- region__sidebar -->
 <?php endif; ?>
