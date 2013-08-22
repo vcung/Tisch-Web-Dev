2d202ae78ce264bf181f7d4e28a73fa3c59c10d0
katebron <kate.bronstad@tufts.edu>
2013-08-22 15:20:25 -0400

    inital footer theming

--- drupal/sites/all/themes/selbert/templates/region--footer.tpl.php
+++ drupal/sites/all/themes/selbert/templates/region--footer.tpl.php
@@ -29,8 +29,42 @@
 <?php if ($content): ?>
   <footer id="footer" class="<?php print $classes; ?>">
     <?php print $content; ?>
-    <p>Tisch Library<br/>
-    	Address<br/>
-    </p>
+    <div id="footerLogo">
+    <img src="tuftslogos.png" alt="Tufts University, Arts & Sciences and Engineering logos"/>
+    </div>
+    <div id="otherFooterLibs">
+    <h3>Other Tufts Libraries</h3>
+    <ul>
+    	<li><a href="">Lilly Music Library</a></li>
+    	<li><a href="">Digital Collections & Archives</a></li>
+    	<li><a href="">Ginn Library</a></li>
+    	<li><a href="">Hirsch Health Sciences Library</a></li>
+    	<li><a href="">Webster Family Library (Veterinary)</a></li>
+    	<li><a href="">GIS Center and Map Room</a></li>
+    </ul>
+    </div>
+    <div id="otherFooterTufts">
+    	<h3>Other Tufts Sites</h3>
+    	<ul>
+    		<li><a href="">Email</a></li>
+            <li><a href="">Trunk</a></li>
+            <li><a href="">iSIS</a></li>
+         </ul>   
+    </div>
+    <div id="footerContact">
+       <h3>Contact Us</h3>
+       <p class="address">Tisch Library</br>
+       	35 Professors Row<br/>
+       	Medford, MA 02155<br/>
+       	</p>
+        <p>617-627-3345</p>
+        <p>Copyright &copy; 2013</p>
+     </div>
+     <div id="footerSocial">
+      <h3>Follow Us</h3>
+      <img src="" alt="twitter icon"/>
+      <img src="" alt="facebook icon"/>
+    </div>
+    	
   </footer><!-- region__footer -->
 <?php endif; ?>
