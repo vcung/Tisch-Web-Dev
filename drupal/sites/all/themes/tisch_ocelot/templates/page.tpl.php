d42610b508c5246efd4d0b47a3700a6b004bcdac
katebron <kate.bronstad@tufts.edu>
2013-09-16 22:47:49 -0400

    fixed messages and tabs

--- drupal/sites/all/themes/tisch_ocelot/templates/page.tpl.php
+++ drupal/sites/all/themes/tisch_ocelot/templates/page.tpl.php
@@ -64,13 +64,14 @@
         <h1 class="page__title title" id="page-title"><?php print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
+      </div> <!--end titleContainer-->
       <?php print $messages; ?>
       <?php print render($tabs); ?>
       <?php print render($page['help']); ?>
       <?php if ($action_links): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
-      </div> <!--end titleContainer-->
+      
       <?php print render($page['content']); ?>
       <?php print $feed_icons; ?>
     </div>
